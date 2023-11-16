3f3d310a24fa7ffa7f3a964767588270e0bd287f
hamayoonk38 <hamayoonk38@gmail.com>
2023-11-16 18:49:13 +0000

    Further validationRules added to the validation middleware

--- middleware/validationRules.js
+++ middleware/validationRules.js
@@ -1,31 +1,31 @@
 const { body, query } = require("express-validator");
 
 const registerValidationRules = () => [
-    body("firstname").notEmpty().withMessage("First name is required"),
-    body("surname").notEmpty().withMessage("Surname is required"),
-    body("email").isEmail().withMessage("Invalid email address"),
-    body("username").notEmpty().withMessage("Username is required"),
-    body("password")
-        .isLength({ min: 8 })
-        .withMessage("Password must be at least 8 characters long"),
+    body("firstname", "First name is required").notEmpty().trim().escape(),
+    body("surname", "Surname is required").notEmpty().trim().escape(),
+    body("email", "Invalid email address").isEmail().normalizeEmail(),
+    body("username", "Username is required").notEmpty().trim().escape(),
+    body("password", "Password must be at least 8 characters long").isLength({
+        min: 8,
+    }),
 ];
 
 const loginValidationRules = () => [
-    body("username", "Username is required").notEmpty(),
+    body("username", "Username is required").notEmpty().trim().escape(),
     body("password", "Password is required").notEmpty(),
 ];
 
 const createPostValidationRules = () => [
-    body("content", "Post content is required").notEmpty(),
+    body("content", "Post content is required").notEmpty().trim().escape(),
 ];
 
 const addCommentValidationRules = () => [
-    body("content", "Comment content is required").notEmpty(),
+    body("content", "Comment content is required").notEmpty().trim().escape(),
 ];
 
 const searchValidationRules = () => [
-    query("query", "Search query is required").notEmpty(),
-    query("type", "Search type is invalid").notEmpty(),
+    query("query", "Search query is required").notEmpty().trim().escape(),
+    query("type", "Search type is invalid").isIn(["users", "posts"]),
 ];
 
 module.exports = {
